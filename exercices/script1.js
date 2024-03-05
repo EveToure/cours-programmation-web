4ff69e1f202396fbb7744f6d273bd7ade4e4ad1c
Eve Tuore <you@example.com>
2024-03-05 18:51:33 +0000

    LAST PUSHING

---
+++ exercices/script1.js
@@ -0,0 +1,15 @@
+// let arr = [64,25,12,22,11]
+// let sortedarr = []
+// let minarr = arr[0]
+
+//     for(let j=0; j<arr.length; j++)  
+
+//       for(let i=0; i<arr.length; i++)
+//         if()
+
+let eltHeader =document. getElementById("nav")
+console.log(eltHeader)
+
+
+
+
---
+++ exercices/script2.js
@@ -0,0 +1,17 @@
+
+function funcUpDownContact(evert){
+    const target = Event.currentTarget
+    console.log(Traget)
+    formElement.inneSrHTML = `<div class="forms"></div>`
+}
+
+
+
+
+
+
+
+
+const contactElement = document.querySelector('.contact')
+contactElement.addEventListener('click', funcUpDownContact)
+console.log(contactElement)
